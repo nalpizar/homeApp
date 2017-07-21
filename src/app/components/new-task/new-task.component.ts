4c72013213f287311c7e89c5b962a849452f14a4
ChecoHerrera <sherrera@atlanticcollegecr.com>
2017-07-21 15:58:12 -0600

    se Agrega ID consecutivo al crea un NewTaskRep

--- src/app/components/new-task/new-task.component.ts
+++ src/app/components/new-task/new-task.component.ts
@@ -12,31 +12,39 @@ export class NewTaskComponent implements OnInit {
 
   private taskRep: TaskRep[] = [];
 
-  title:string = 'title';
-  weight:number = 0;
+  title: string = 'title';
+  weight: number = 0;
 
-  onTitleChange(event){
+  onTitleChange(event) {
     this.title = event.target.value;
   }
 
-  onWeightChange(event){
+  onWeightChange(event) {
     this.weight = event.target.value;
   }
 
-  addNewTask () {
-    let temptask = new TaskRep( 0, this.title, this.weight );
+  addNewTask() {
+    let index = this.family.getTaskRep().length;
+    console.log(index);
+    index--;
+    console.log(index);
+    let newId = this.family.getTaskRep()[index].getId();
+    console.log(newId);
+    newId++;
+    let temptask = new TaskRep(newId, this.title, this.weight);
     this.family.addTaskRep(temptask);
     this.title = 'title';
+    console.log(this.family.getTaskRep())
   }
 
-  weights:any[] = [
-    {id:1,name:'1'},
-    {id:2,name:'2'},
-    {id:3,name:'3'},
-    {id:4,name:'4'},
-    {id:5,name:'5'}
+  weights: any[] = [
+    { id: 1, name: '1' },
+    { id: 2, name: '2' },
+    { id: 3, name: '3' },
+    { id: 4, name: '4' },
+    { id: 5, name: '5' }
   ];
- 
+
   constructor() { }
 
   ngOnInit() {
