9aadfa3033cf59826de53f134bb3fa1ede28619c
David Fallas <dfallasr@ucenfotec.ac.cr>
2017-07-25 14:26:39 -0600

    Calificar estrellas funcionalidad completa

--- src/app/components/stars/stars.component.ts
+++ src/app/components/stars/stars.component.ts
@@ -16,6 +16,57 @@ export class StarsComponent implements OnInit {
   ngOnInit() {
   }
 
+  emptyStar = true;
+  emptyStar2 = true; 
+  emptyStar3 = true; 
+  emptyStar4 = true; 
+  emptyStar5 = true;
+
+  
+
+  setStar1(){
+      let classes = {
+          "fa-star-o": this.emptyStar,
+          "fa-star": !this.emptyStar
+      };
+      return classes;
+  }
+
+  setStar2(){
+      let classes = {
+          "fa-star-o": this.emptyStar2,
+          "fa-star": !this.emptyStar2
+      };
+      return classes;
+  }
+
+  setStar3(){
+      let classes = {
+          "fa-star-o": this.emptyStar3,
+          "fa-star": !this.emptyStar3
+      };
+      return classes;
+  }
+
+  setStar4(){
+      let classes = {
+          "fa-star-o": this.emptyStar4,
+          "fa-star": !this.emptyStar4
+      };
+      return classes;
+  }
+
+  setStar5(){
+      let classes = {
+          "fa-star-o": this.emptyStar5,
+          "fa-star": !this.emptyStar5
+      };
+      return classes;
+  }
+
+
+
+
   rateScore(value){
 
     var currentStars = this.task.getScore().getStars();
@@ -25,20 +76,35 @@ export class StarsComponent implements OnInit {
       currentStar.setStatus(false);
     }
 
+    this.emptyStar = true;
+    this.emptyStar2 = true;
+    this.emptyStar3 = true;
+    this.emptyStar4 = true;
+    this.emptyStar5 = true;
+    
+
     switch(value) { 
       case 1: { 
           currentStars[0].setStatus(true);
+          this.emptyStar = false;
+
           break; 
       }
       case 2: { 
           currentStars[0].setStatus(true);
           currentStars[1].setStatus(true);
+          this.emptyStar = false;
+          this.emptyStar2 = false;
+
           break; 
       }
       case 3: { 
           currentStars[0].setStatus(true);
           currentStars[1].setStatus(true);
           currentStars[2].setStatus(true);
+          this.emptyStar = false;
+          this.emptyStar2 = false;
+          this.emptyStar3 = false;
           break; 
       }
       case 4: { 
@@ -46,6 +112,11 @@ export class StarsComponent implements OnInit {
           currentStars[1].setStatus(true);
           currentStars[2].setStatus(true);
           currentStars[3].setStatus(true);
+          this.emptyStar = false;
+          this.emptyStar2 = false;
+          this.emptyStar3 = false;
+          this.emptyStar4 = false;
+
           break; 
       }
       case  5: { 
@@ -54,6 +125,12 @@ export class StarsComponent implements OnInit {
           currentStars[2].setStatus(true);
           currentStars[3].setStatus(true);
           currentStars[4].setStatus(true);
+          this.emptyStar = false;
+          this.emptyStar2 = false;
+          this.emptyStar3 = false;
+          this.emptyStar4 = false;
+          this.emptyStar5 = false;
+
           break; 
       }
     }
