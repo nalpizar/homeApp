0da153d21ad63188c88dc607b728837450a8c6e1
ChecoHerrera <sherrera@atlanticcollegecr.com>
2017-08-04 21:57:38 -0600

    Login Funcionando

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -17,20 +17,28 @@ export class SignInComponent implements OnInit {
 
   ngOnInit() {
     this.auth.getAuthState().subscribe(
-    (user) => this.user = user);
+      (user) => this.user = user);
   }
 
   loginWithGoogle() {
-    this.auth.loginWithGoogle();
-    if (this.auth.afAuth.auth.currentUser != null) {
-      this.sendCont.emit(4);
+    console.log(this.user)
+    if (this.user == null) {
+      this.auth.loginWithGoogle();
     } else {
-      this.sendCont.emit(0);
+      this.sendCont.emit(4);
     }
+    let temp = setInterval(() => {    //<<<---    using ()=> syntax
+      console.log(this.user)
+      if (this.user != null) {
+        this.sendCont.emit(4);
+        clearInterval(temp);
+      }
+    }, 1000);
   }
 
-  logOut () {
+  logOut() {
     this.auth.afAuth.auth.signOut();
-  }  
+    console.log(this.user)
+  }
 
 }
