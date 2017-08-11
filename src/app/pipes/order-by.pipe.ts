b6db8d22a6676a577a5b7e686721b57ae1ec5ddd
David Fallas <dfallasr@ucenfotec.ac.cr>
2017-08-11 07:58:36 -0600

    Cuando la tarea este completa que se vaya para abajo

--- src/app/pipes/order-by.pipe.ts
+++ src/app/pipes/order-by.pipe.ts
@@ -3,16 +3,17 @@ import { Pipe, PipeTransform } from "@angular/core";
 name: 'orderBy'
 } )
 export class OrderByPipe implements PipeTransform {
-transform( array: Array<any>, orderField: string, orderType: boolean ): Array<string> {
-    array.sort( ( a: any, b: any ) => {
-        let ae = a[ orderField ];
-        let be = b[ orderField ];
-        if ( ae == undefined && be == undefined ) return 0;
-        if ( ae == undefined && be != undefined ) return orderType ? 1 : -1;
-        if ( ae != undefined && be == undefined ) return orderType ? -1 : 1;
-        if ( ae == be ) return 0;
-        return orderType ? (ae.toString().toLowerCase() > be.toString().toLowerCase() ? -1 : 1) : (be.toString().toLowerCase() > ae.toString().toLowerCase() ? -1 : 1);
-    } );
+  transform(array: any[], field: string): any[] {
+    array.sort((a: any, b: any) => {
+      if (a[field] < b[field]) {
+        return -1;
+      } else if (a[field] > b[field]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    })
+    console.log(array);
     return array;
   }
 }
